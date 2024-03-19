79a4fb49c667a2347b841c582f17063bcb5cfc0b
LinsRuis.wubt20 <mgr9525@qq.com>
2024-03-19 14:52:10 +0800

    conv_human_readable

--- src/bytes/mod.rs
+++ src/bytes/mod.rs
@@ -1,6 +1,6 @@
 pub use bytes::ByteBox;
 pub use bytes::ByteBoxBuf;
-#[cfg(feature="asyncs")]
+#[cfg(feature = "asyncs")]
 pub use bytes::ByteSteamBuf;
 pub use circle::CircleBuf;
 pub use stream::*;
@@ -9,22 +9,32 @@ mod bytes;
 mod circle;
 mod stream;
 
-
+pub fn conv_human_readable(sz: usize) -> String {
+    let szf = sz as f64;
+    if sz >= 1024 * 1024 * 1024 {
+        return format!("{:.2}GB", szf / 1024f64 / 1024f64 / 1024f64);
+    } else if sz >= 1024 * 1024 {
+        return format!("{:.2}MB", szf / 1024f64 / 1024f64);
+    } else if sz >= 1024 {
+        return format!("{:.2}KB", szf / 1024f64);
+    }
+    return format!("{}B", sz);
+}
 
 #[cfg(test)]
 mod tests {
     use super::ByteBoxBuf;
 
-  #[test]
-  fn it_works() {
-      assert_eq!(2 + 2, 4);
-  }
-  #[test]
-  fn bufs() {
-      let mut buf=ByteBoxBuf::new();
-      buf.push(vec![0x8e,0x8f]);
-      buf.push(vec![0xff,0xa7,0x33]);
-      let bts=buf.to_bytes();
-      println!("datas({}/{}):{:?}",bts.len(),buf.len(),&bts[..]);
-  }
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+    #[test]
+    fn bufs() {
+        let mut buf = ByteBoxBuf::new();
+        buf.push(vec![0x8e, 0x8f]);
+        buf.push(vec![0xff, 0xa7, 0x33]);
+        let bts = buf.to_bytes();
+        println!("datas({}/{}):{:?}", bts.len(), buf.len(), &bts[..]);
+    }
 }
