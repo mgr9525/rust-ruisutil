4465a0f38455ac1fe174293c4fd00bae0511b322
LinsRuis.HW32G1 <mgr9525@qq.com>
2024-07-25 17:02:44 +0800

    +

--- src/utils.rs
+++ src/utils.rs
@@ -67,9 +67,9 @@ pub fn byte2struct<T: Sized>(p: &mut T, bts: &[u8]) -> io::Result<usize> {
     Ok(ln)
 }
 
-pub fn parse_noip_addr<T: AsRef<str>>(s: T) -> std::io::Result<net::SocketAddr> {
+pub fn parse_noip_addr<T: AsRef<str>>(s: T) -> String {
     let idx = s.as_ref().rfind(':');
-    let hs = match idx {
+    match idx {
         Some(i) => {
             if i == 0 {
                 format!("0.0.0.0{}", &s.as_ref()[i..])
@@ -79,8 +79,7 @@ pub fn parse_noip_addr<T: AsRef<str>>(s: T) -> std::io::Result<net::SocketAddr> 
             }
         }
         None => format!("{}:0", s.as_ref()),
-    };
-    net::SocketAddr::from_str(&hs).map_err(|e| ioerr(format!("parse addr err:{}", e), None))
+    }
 }
 pub fn tcp_read(ctx: &Context, stream: &mut net::TcpStream, ln: usize) -> io::Result<Box<[u8]>> {
     if ln <= 0 {
